d7302a6fee6b2a35d7e926d2f772f202b5a177cd
Raita Kudo <qdrait@gmail.com>
2018-09-06 02:55:20 +0000

    heroku sendgrid

--- app/mailers/post_mailer.rb
+++ app/mailers/post_mailer.rb
@@ -1,6 +1,6 @@
 class PostMailer < ApplicationMailer
     def post_mail(post)
          @post = post
-         mail to: "test@test.com", subject: "投稿完了のお知らせ"
+         mail to: @post.user.email, subject: "投稿完了のお知らせ"
     end
 end
